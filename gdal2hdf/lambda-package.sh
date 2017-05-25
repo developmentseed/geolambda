c252cebe872132512cef6234737fe39c557e678d
Matthew Hanson <matt.a.hanson@gmail.com>
2017-05-25 16:31:34 -0400

    updated install locations

--- gdal2hdf/lambda-package.sh
+++ gdal2hdf/lambda-package.sh
@@ -2,10 +2,6 @@
 
 # directory used for deployment
 DEPLOY_DIR=lambda
-# where packages were built
-BUILD_DIR=/build
-# where user installed libraries found
-PRE=/usr/local
 
 # install app
 pip install .
@@ -13,20 +9,20 @@ cp lambda_handler.py $DEPLOY_DIR/
 
 # copy libs
 mkdir -p $DEPLOY_DIR/lib/python2.7/site-packages
-cp $BUILD_DIR/lib/libproj.so* $DEPLOY_DIR/lib/
-cp $BUILD_DIR/lib/libgdal.so* $DEPLOY_DIR/lib/
-cp $BUILD_DIR/lib/libdf.so.0 $DEPLOY_DIR/lib/
-cp $BUILD_DIR/lib/libsz.so.2 $DEPLOY_DIR/lib/
-cp $BUILD_DIR/lib/libhdf5.so.10 $DEPLOY_DIR/lib/
-cp $BUILD_DIR/lib/libmfhdf.so.0 $DEPLOY_DIR/lib/
-rsync -ax $PRE/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $PRE/etc/lambda-excluded-packages
+cp $PREFIX/lib/libproj.so* $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libgdal.so* $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libdf.so.0 $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libsz.so.2 $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libhdf5.so.10 $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libmfhdf.so.0 $DEPLOY_DIR/lib/
+rsync -ax $PREFIX/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # copy 64-bit libs
 mkdir -p $DEPLOY_DIR/lib64/python2.7/site-packages
 cp /usr/lib64/libgeos_c.so.1 $DEPLOY_DIR/lib64/
 cp /usr/lib64/libgeos-3.4.2.so $DEPLOY_DIR/lib64/
 #cp /usr/lib64/libpq.so.5 $DEPLOY_DIR/lib/
-rsync -ax $PRE/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $PRE/etc/lambda-excluded-packages
+rsync -ax $PREFIX/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # zip up deploy package
 cd $DEPLOY_DIR
