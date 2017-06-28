126cd3f82ecd4efaaa0a6ad4a46678493b80b96f
Matthew Hanson <matt.a.hanson@gmail.com>
2017-06-28 13:21:03 -0400

    update packaging for new version of libhdf5

--- gdal2hdf/lambda-package.sh
+++ gdal2hdf/lambda-package.sh
@@ -13,7 +13,7 @@ cp $PREFIX/lib/libproj.so* $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libgdal.so* $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libdf.so.0 $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libsz.so.2 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libhdf5.so.10 $DEPLOY_DIR/lib/
+cp $PREFIX/lib/libhdf5.so.101 $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libmfhdf.so.0 $DEPLOY_DIR/lib/
 rsync -ax $PREFIX/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
 
