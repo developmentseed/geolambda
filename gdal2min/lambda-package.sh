d2bd2cd247600845b3f04000fac3adee0c372bda
Matthew Hanson <matt.a.hanson@gmail.com>
2017-06-28 17:16:59 -0400

    update packaging scripts

--- gdal2min/lambda-package.sh
+++ gdal2min/lambda-package.sh
@@ -5,20 +5,21 @@ DEPLOY_DIR=lambda
 
 # install app
 pip install .
+
+# make deployment directory and add lambda handler
+mkdir -p $DEPLOY_DIR/lib/python2.7/site-packages
 cp lambda_handler.py $DEPLOY_DIR/
 
-# copy libs
-mkdir -p $DEPLOY_DIR/lib/python2.7/site-packages
+# copy 32-bit libs
 cp $PREFIX/lib/libproj.so* $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libgdal.so* $DEPLOY_DIR/lib/
 rsync -ax $PREFIX/lib/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # copy 64-bit libs
-mkdir -p $DEPLOY_DIR/lib64/python2.7/site-packages
-cp /usr/lib64/libgeos_c.so.1 $DEPLOY_DIR/lib64/
-cp /usr/lib64/libgeos-3.4.2.so $DEPLOY_DIR/lib64/
-rsync -ax $PREFIX/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib64/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
+cp /usr/lib64/libgeos_c.so.1 $DEPLOY_DIR/lib/
+cp /usr/lib64/libgeos-3.4.2.so $DEPLOY_DIR/lib/
+rsync -ax $PREFIX/lib64/python2.7/site-packages/ $DEPLOY_DIR/lib/python2.7/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # zip up deploy package
 cd $DEPLOY_DIR
-zip -ruq ../deploy.zip ./ -x excluded_packages
+zip -ruq ../deploy.zip ./
