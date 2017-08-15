7187b10459f6b0edb1508f798a6e033f20312093
Matthew Hanson <matt.a.hanson@gmail.com>
2017-08-15 00:17:10 -0400

    example lambda program + tests

--- geolambda-seed/lambda/lambda_handler.py
+++ geolambda-seed/lambda/lambda_handler.py
@@ -12,7 +12,20 @@ logger.setLevel(logging.DEBUG)
 # commented out to avoid duplicate logs in lambda
 # logger.addHandler(logging.StreamHandler())
 
+# imports used for the example code below
+from osgeo import gdal
+
 
 def handler(event, context):
-    # process event payload and do something
-    return payload
+    """ Lambda handler """
+    logger.debug(event)
+
+    # process event payload and do something like this
+    fname = event['filename']
+    fname = fname.replace('s3://', '/vsis3/')
+    # open and return metadata
+    ds = gdal.Open(fname)
+    band = ds.GetRasterBand(1)
+    stats = band.GetStatistics(0, 1)
+
+    return stats
