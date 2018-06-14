b156a2b643fda9bcf4b0d3523eb3b2dac39062f6
Matthew Hanson <matt.a.hanson@gmail.com>
2018-06-14 18:31:25 -0400

    pass arguments to base packaging script

--- bin/lambda-package-base.sh
+++ bin/lambda-package-base.sh
@@ -5,6 +5,8 @@ DEPLOY_DIR=lambda
 
 PYVER=${1:-2.7}
 
+echo PYVER: $PYVER
+
 # make deployment directory and add lambda handler
 mkdir -p $DEPLOY_DIR/lib/python$PYVER/site-packages
 
@@ -19,12 +21,14 @@ cp $PREFIX/lib/libmfhdf.so.0 $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libgeos_c.so.1 $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libgeos-3.6.2.so $DEPLOY_DIR/lib/
 cp $PREFIX/lib/libnetcdf.so.13 $DEPLOY_DIR/lib/
-rsync -ax $PREFIX/lib/python$PYVER/site-packages/ $DEPLOY_DIR/lib/python$PYVER/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
+rsync -ax $PREFIX/lib/python$PYVER/site-packages/ $DEPLOY_DIR/lib/python$PYVER/site-packages/ \
+    --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # copy 64-bit libs
 cp /usr/lib64/libjpeg.so.62 $DEPLOY_DIR/lib/
 #cp /usr/lib64/libpq.so.5 $DEPLOY_DIR/lib/
-rsync -ax $PREFIX/lib64/python$PYVER/site-packages/ $DEPLOY_DIR/lib/python$PYVER/site-packages/ --exclude-from $PREFIX/etc/lambda-excluded-packages
+rsync -ax $PREFIX/lib64/python$PYVER/site-packages/ $DEPLOY_DIR/lib/python$PYVER/site-packages/ \
+    --exclude-from $PREFIX/etc/lambda-excluded-packages
 
 # zip up deploy package
 cd $DEPLOY_DIR
