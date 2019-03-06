4f746aa5e8b639908e2ce0d31df93359f6367fd3
Matthew Hanson <matt.a.hanson@gmail.com>
2019-03-06 17:26:23 -0500

    update lambda_function with a default filename, for testing

--- python/lambda/lambda_function.py
+++ python/lambda/lambda_function.py
@@ -12,12 +12,15 @@ logger.setLevel(logging.DEBUG)
 from osgeo import gdal
 
 
+test_filename = 'https://landsat-pds.s3.amazonaws.com/c1/L8/086/240/LC08_L1GT_086240_20180827_20180827_01_RT/LC08_L1GT_086240_20180827_20180827_01_RT_B1.TIF'
+
+
 def lambda_handler(event, context=None):
     """ Lambda handler """
     logger.debug(event)
 
     # process event payload and do something like this
-    fname = event['filename']
+    fname = event.get('filename', test_filename)
     fname = fname.replace('s3://', '/vsis3/')
     # open and return metadata
     ds = gdal.Open(fname)
@@ -29,8 +32,6 @@ def lambda_handler(event, context=None):
 
 if __name__ == "__main__":
     """ Test lambda_handler """
-    event = {
-        'filename': 'https://landsat-pds.s3.amazonaws.com/c1/L8/086/240/LC08_L1GT_086240_20180827_20180827_01_RT/LC08_L1GT_086240_20180827_20180827_01_RT_B1.TIF'
-    }
+    event = {'filename': test_filename}
     stats = lambda_handler(event)
     print(stats)
