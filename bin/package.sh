f9b18fc05bd6719421abda2b9f5841a1cf5b6a3b
Matthew Hanson <matt.a.hanson@gmail.com>
2019-03-13 18:55:54 -0400

    simplify packaging, package all libs in /usr/local/lib

--- bin/package.sh
+++ bin/package.sh
@@ -8,25 +8,8 @@ echo Creating deploy package
 # make deployment directory and add lambda handler
 mkdir -p $DEPLOY_DIR/lib
 
-# copy 32-bit libs
-cp $PREFIX/lib/libproj.so.13 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libgdal.so.20 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libdf.so.0 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libsz.so.2 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libhdf5.so.103 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libhdf5_hl.so.100 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libmfhdf.so.0 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libgeos_c.so.1 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libgeos-3.7.1.so $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libnetcdf.so.13 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libopenjp2.so.7 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libwebp.so.7 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libgeotiff.so.2 $DEPLOY_DIR/lib/
-cp $PREFIX/lib/libzstd.so.1 $DEPLOY_DIR/lib/
-
-# copy 64-bit libs
-cp /usr/lib64/libjpeg.so.62 $DEPLOY_DIR/lib/
-#cp /usr/lib64/libpq.so.5 $DEPLOY_DIR/lib/
+# copy libs
+cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
 
 strip $DEPLOY_DIR/lib/* || true
 
