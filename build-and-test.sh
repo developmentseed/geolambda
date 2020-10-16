60f3406b4dd9be2c861da19d06ba76efb95fdcb6
Matthew Hanson <matt.a.hanson@gmail.com>
2020-10-16 19:11:30 -0400

    set envvars when running test docker

--- build-and-test.sh
+++ build-and-test.sh
@@ -9,4 +9,5 @@ cd python
 docker build . --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
 docker run -v ${PWD}:/home/geolambda -t developmentseed/geolambda:${VERSION}-python package-python.sh
 
-docker run --rm -v ${PWD}/lambda:/var/task -v ${PWD}/../lambda:/opt lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
+docker run -e GDAL_DATA=/opt/share/gdal -e PROJ_LIB=/opt/share/proj \
+    --rm -v ${PWD}/lambda:/var/task -v ${PWD}/../lambda:/opt lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
