8a7aee46ea07cb0b8d1ab46328a64a945dca2132
Matthew Hanson <matt.a.hanson@gmail.com>
2020-10-17 00:29:40 -0400

    include base geolambda files in python geolambda

--- python/bin/package-python.sh
+++ python/bin/package-python.sh
@@ -3,6 +3,20 @@
 # directory used for development
 export DEPLOY_DIR=lambda
 
+# make deployment directory and add lambda handler
+mkdir -p $DEPLOY_DIR/lib
+
+# copy libs
+cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
+cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
+
+strip $DEPLOY_DIR/lib/* || true
+
+# copy GDAL_DATA files over
+mkdir -p $DEPLOY_DIR/share
+rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
+rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
+
 # Get Python version
 PYVERSION=$(cat /root/.pyenv/version)
 MAJOR=${PYVERSION%%.*}
